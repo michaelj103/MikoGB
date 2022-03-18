e7d3548e5731eb740d9421c075381cc8a235ff64
Michael Brandt <michaeljbrandt@icloud.com>
2022-03-18 12:31:58 -0700

    added iOS target

---
+++ MikoGB-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MikoGB-iOS
+//
+//  Created by Michael Brandt on 3/16/22.
+//  Copyright © 2022 Michael Brandt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
