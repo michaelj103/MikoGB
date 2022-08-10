0e26ecd35343fba65fc0afac47ba20a4d53de6d6
Michael Brandt <michaeljbrandt@icloud.com>
2022-08-10 14:02:23 -0700

    bump build numbers

--- MikoGB-iOS/UpdateManager.swift
+++ MikoGB-iOS/UpdateManager.swift
@@ -10,9 +10,9 @@ import Foundation
 import GBServerPayloads
 
 class UpdateManager {
-    private static let BuildVersion = 5
+    private static let BuildVersion = 6
     private static let BuildVersionKey = "BuildVersion"
-    private static let CurrentVersion = "v0.8.3"
+    private static let CurrentVersion = "v0.8.4"
     
     static func getCurrentVersionAndBuild() -> (String, Int) {
         return (CurrentVersion, BuildVersion)
