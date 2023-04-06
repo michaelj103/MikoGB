01835afe2e0b1123e120bf402cc46d14b436d902
Michael Brandt <michaeljbrandt@icloud.com>
2023-04-05 21:29:35 -0700

    rebuild and bump version for new provisioning profile

--- Shared/UpdateManager.swift
+++ Shared/UpdateManager.swift
@@ -10,9 +10,9 @@ import Foundation
 import GBServerPayloads
 
 class UpdateManager {
-    private static let BuildVersion = 13
+    private static let BuildVersion = 14
     private static let BuildVersionKey = "BuildVersion"
-    private static let CurrentVersion = "v0.10.3"
+    private static let CurrentVersion = "v0.10.4"
     
     static func getCurrentVersionAndBuild() -> (String, Int) {
         return (CurrentVersion, BuildVersion)
